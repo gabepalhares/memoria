b8ddf877c28d93cb911915f728eb4e94875bb597
minortypo <gabrielpalharesdev@gmail.com>
2024-09-02 23:16:48 -0300

    feat: updates head and og description

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,6 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
 
-class MyDocument extends Document {
+export class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
@@ -8,19 +8,19 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <meta
             name="description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="See pictures from Gabriel Palhares' personal gallery."
           />
-          <meta property="og:site_name" content="nextjsconf-pics.vercel.app" />
+          <meta property="og:site_name" content="m-emoria.vercel.app" />
           <meta
             property="og:description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="See pictures from Gabriel Palhares' personal gallery."
           />
-          <meta property="og:title" content="Next.js Conf 2022 Pictures" />
+          <meta property="og:title" content="Gabriel Palhares' personal gallery" />
           <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:title" content="Next.js Conf 2022 Pictures" />
+          <meta name="twitter:title" content="Gabriel Palhares' personal gallery" />
           <meta
             name="twitter:description"
-            content="See pictures from Next.js Conf and the After Party."
+            content="See pictures from Gabriel Palhares' personal gallery."
           />
         </Head>
         <body className="bg-black antialiased">
@@ -31,5 +31,3 @@ class MyDocument extends Document {
     );
   }
 }
-
-export default MyDocument;
